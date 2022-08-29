0c3e02c10cca9513bdfb312b1521c37f3758274e
tuguldur <tuguldur416@example.com>
2022-08-29 16:31:42 +0800

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,15 @@
+# n=int(input())
+# s=0
+# for i in range(1,n+1):
+#     if(n % i ==0):
+#         s=s + i
+# print(s)
+from re import S
+
+
+n=int(input())
+
+for i in range(1,n*1):
+    if(n*1 ):
+        s=s + i
+        print(s)
